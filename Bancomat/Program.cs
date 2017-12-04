84bf268b8796b9c21d04dfb512475691a9327b6c
Dias29 <33831288+Dias29@users.noreply.github.com>
2017-12-04 23:02:55 +0600

    first

---
+++ Bancomat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Banking;
+using Corporation;
+using Person;
+
+namespace Bancomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
